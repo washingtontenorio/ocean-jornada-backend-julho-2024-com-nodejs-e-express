623cd4ec88a7f7d75145378272c04789b9e6db53
washingtontenorio <105123643+washingtontenorio@users.noreply.github.com>
2024-07-03 14:15:49 +0000

    Endpoint Create finalizado

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express()
-
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
 // Desafio: criar endpoint /oi que exibe "Olá, mundo!"
 app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
-
-// lista de personagens
-
-const lista = ['Rick Sanchez','Morty Smith','Summer Smith']
-
-// Read All 
-
-app.get('/item', function(req, res){
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+// Read All - [GET] /item
+app.get('/item', function (req, res) {
+  // Pegamos a lista e enviamos como resposta HTTP
   res.send(lista)
+})
+// Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json())
 
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+  console.log(req.body)
+  res.send('Create')
+  // Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+
+  // Inserimos o item no final da lista
+  lista.push(item)
+
+  // Enviamos uma mensagem de sucesso!
+  res.send('Item criado com sucesso!')
 })
 
 app.listen(3000)
