3c4f16bbbbb0a88a6e408a9e01223577063609c9
washingtontenorio <105123643+washingtontenorio@users.noreply.github.com>
2024-07-03 12:56:06 +0000

    Endpoint Read All

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
+app.get('/oi', function (req, res) {
+  res.send('Olá, mundo!')
+})
+
+// lista de personagens
+
+const lista = ['Rick Sanchez','Morty Smith','Summer Smith']
+
+// Read All 
+
+app.get('/item', function(req, res){
+  res.send(lista)
+
+})
+
 app.listen(3000)
